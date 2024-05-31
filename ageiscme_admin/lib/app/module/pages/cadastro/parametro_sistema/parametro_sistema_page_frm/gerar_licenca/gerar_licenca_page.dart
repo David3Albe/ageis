9e80497742924cd4af38abb9af4d59ccaa638c8d
Alessandro Eich <alessandro.eich@movtech.com.br>
2024-05-31 13:15:15 -0300

    Desenvolvimento ageiscme v2

---
+++ ageiscme_admin/lib/app/module/pages/cadastro/parametro_sistema/parametro_sistema_page_frm/gerar_licenca/gerar_licenca_page.dart
@@ -0,0 +1,11 @@
+import 'package:dependencias_comuns/bloc_export.dart';
+import 'package:flutter/material.dart';
+
+class GerarLicencaPage extends StatelessWidget {
+  const GerarLicencaPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(providers: [], child: child);
+  }
+}
---
+++ ageiscme_admin/lib/app/module/pages/cadastro/parametro_sistema/parametro_sistema_page_frm/gerar_licenca/page/gerar_licenca_widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class GerarLicencaWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+
+}
