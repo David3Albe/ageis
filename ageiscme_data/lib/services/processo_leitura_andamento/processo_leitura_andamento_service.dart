a833b4f9eabbde8855ef3946243a70ef2e40ad08
alessandro.eich <alessandro.eich@movtech.com.br>
2024-09-18 13:48:43 -0300

    Adicionado detalhamento das leituras em andamento

--- ageiscme_data/lib/services/processo_leitura_andamento/processo_leitura_andamento_service.dart
+++ ageiscme_data/lib/services/processo_leitura_andamento/processo_leitura_andamento_service.dart
@@ -1,4 +1,6 @@
 import 'package:ageiscme_data/shared/custom_dio.dart';
+import 'package:ageiscme_models/dto/processo_leitura_andamento/detail/filter/processo_leitura_andamento_detail_filter_dto.dart';
+import 'package:ageiscme_models/dto/processo_leitura_andamento/detail/processo_leitura_andamento_detail_dto.dart';
 import 'package:ageiscme_models/filters/processo_leitura_andamento/processo_leitura_andamento_filter.dart';
 import 'package:ageiscme_models/models/processo_leitura_andamento/processo_leitura_andamento_model.dart';
 
@@ -19,9 +21,22 @@ class ProcessoLeituraAndamentoService {
   Future<List<ProcessoLeituraAndamentoModel>> FilterNoLock(
     ProcessoLeituraAndamentoFilter filter,
   ) async =>
-      (await _client.postList('/processo-leitura-andamento/filter-no-lock', filter))
+      (await _client.postList(
+        '/processo-leitura-andamento/filter-no-lock',
+        filter,
+      ))
           .map((e) => ProcessoLeituraAndamentoModel.fromJson(e))
           .toList();
+
+  Future<(String message, ProcessoLeituraAndamentoDetailDTO leituraAndamento)?>
+      Detail(
+    ProcessoLeituraAndamentoDetailFilterDTO filter,
+  ) async =>
+          await _client.post(
+            '/processo-leitura-andamento/detail',
+            filter,
+            (dynamic json) => ProcessoLeituraAndamentoDetailDTO.fromJson(json),
+          );
 
   Future<(String message, ProcessoLeituraAndamentoModel leituraAndamento)?>
       delete(
